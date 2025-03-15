c3567921c22dfc4ceb49efc836185d143ee47c38
Nicolás Medina <niemep@gmail.com>
2025-03-15 16:32:15 -0300

    commit inicial

---
+++ src/main/java/io/apiDevelopment/grupo2/proyectoFinal/ProyectoFinalApplication.java
@@ -0,0 +1,13 @@
+package io.apiDevelopment.grupo2.proyectoFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalApplication.class, args);
+	}
+
+}
