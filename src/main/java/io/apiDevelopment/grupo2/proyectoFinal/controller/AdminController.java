af0b4bd1eb9f8db850db7cc6ee657eedfd69770f
Elmer Mamani <elmer.mamani.urrelo@gmail.com>
2025-03-15 18:44:30 -0300

    Se agrega logica al model, repository, service y controller de Admin.

--- src/main/java/io/apiDevelopment/grupo2/proyectoFinal/controller/AdminController.java
+++ src/main/java/io/apiDevelopment/grupo2/proyectoFinal/controller/AdminController.java
@@ -1,5 +1,25 @@
 package io.apiDevelopment.grupo2.proyectoFinal.controller;
 
-public class AdminController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import io.apiDevelopment.grupo2.proyectoFinal.model.Admin;
+import io.apiDevelopment.grupo2.proyectoFinal.service.AdminService;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+public class AdminController {
+	
+	@Autowired
+	private AdminService adminService;
+	
+	@PostMapping("/login")
+	public ResponseEntity<Admin> getAdminByUsername(@RequestBody Admin admin){
+		return new ResponseEntity<Admin>(adminService.getAdminByUsername(admin), HttpStatus.ACCEPTED);
+	}
 }
--- src/main/java/io/apiDevelopment/grupo2/proyectoFinal/model/Admin.java
+++ src/main/java/io/apiDevelopment/grupo2/proyectoFinal/model/Admin.java
@@ -1,8 +1,22 @@
 package io.apiDevelopment.grupo2.proyectoFinal.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
 
+@Getter
 @Entity
+@Table(name = "admin")
 public class Admin {
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer  id;
+	
+	private String username;
+	
+	private String password;
 }
